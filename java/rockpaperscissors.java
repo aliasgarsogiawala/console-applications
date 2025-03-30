e21312beba669f7df68a4702324632d0e11daf01
Aliasgar Sogiawala <vertexxinfotech18@gmail.com>
2025-03-30 17:06:05 +0530

    Added Programming Language folders

--- java/rockpaperscissors.java
+++ java/rockpaperscissors.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class rockpaperscrissors{
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("")
+        System.out.println("Welcome to Rock Paper Scissors!");
     }
 }
